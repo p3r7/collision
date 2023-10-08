6cf23a5521d63a21b5b052e452b604706cf7cb1b
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-08 10:36:33 +0200

    comments + tweaks

--- collision.lua
+++ collision.lua
@@ -1,5 +1,7 @@
 -- collision.
 -- @eigen
+--
+-- inspired by the visualisations for the ATLAS experiments at CERN's LHC.
 
 
 -- -------------------------------------------------------------------------
@@ -11,6 +13,7 @@ elseif norns then
   FPS = 15
 end
 
+-- time in seconds between each collision
 BANG_INTERVAL_S = 2
 
 function screen_size()
@@ -28,7 +31,6 @@ end
 COL_BG = {0, 0, 0}
 COL_FLASH = {250, 250, 250}
 COL_DETECTOR_BG = {25, 21, 13}
--- COL_DETECTOR_BG = {13, 25, 21}
 COL_PARTICLE = {255, 165, 0}
 COL_BARS = {253, 238, 0}
 COL_MUON = {255, 0, 0}
@@ -37,9 +39,24 @@ COL_RIPPLE = {110, 105, 120}
 
 
 -- -------------------------------------------------------------------------
+-- fading speed
+
+-- how much time elements stay on screen between each bang
+-- at 1, it will fade during the full duration between 2 collisions
+-- at 1/2, it will stay for half this duration
+-- at 2 it will only fade by half
+
+FADE_T_INNER_RIPPLES = 1/1.5
+FADE_T_RIPPLES = 1/1.5
+FADE_T_PARTICLES = 1/4
+FADE_T_HADRONS = 1/2
+FADE_T_MUONS = 4
+
+
+-- -------------------------------------------------------------------------
 -- state
 
-screen_dirty = true
+has_banged = false
 
 frame_count = 1
 
@@ -130,11 +147,8 @@ function init()
   clock_redraw = clock.run(function()
       while true do
         clock.sleep(1/FPS)
-        -- if screen_dirty or pct_time_bang() < 0.5 then
-        if true then
           redraw()
-        end
-        frame_count = frame_count + 1
+          frame_count = frame_count + 1
       end
   end)
 
@@ -143,7 +157,7 @@ function init()
         clock.sleep(BANG_INTERVAL_S)
         bang()
         frame_count = 1
-        screen_dirty = true
+        has_banged = true
       end
   end)
 end
@@ -164,18 +178,24 @@ function redraw()
 
   local pct_bang = pct_time_bang()
 
-  screen.move(center_x, center_y)
+  if has_banged and frame_count < 5 then
+    -- flash
+    screen.move(1, 1)
+    screen.color(table.unpack(COL_FLASH))
+    screen.rect_fill(screen_w, screen_h)
+  else
+    -- detector bg
+    screen.move(center_x, center_y)
+    screen.color(table.unpack(COL_DETECTOR_BG))
+    screen.circle_fill(outer_r)
+    screen.color(table.unpack(COL_BG))
+    screen.circle_fill(inner_r)
+  end
 
-  -- detector bg
-  screen.color(table.unpack(COL_DETECTOR_BG))
-  screen.circle_fill(outer_r)
-  screen.color(table.unpack(COL_BG))
-  screen.circle_fill(inner_r)
-
-  -- inner ripples
+  -- inner (concentric) ripples
   if tab.count(muons) > 0 then
     local inner_ripples = {1/9, 1/2, 1/2 + 1/8, 1/2 + 2/8, 1/2 + 3/8}
-    screen.color(table.unpack(color_scale(COL_BARS, COL_BG, pct_bang * 1.5)))
+    screen.color(table.unpack(color_scale(COL_BARS, COL_BG, pct_bang / FADE_T_INNER_RIPPLES)))
     for _, r_ratio in pairs(inner_ripples) do
       local r = inner_r * r_ratio
       for i=1,70 do
@@ -189,15 +209,18 @@ function redraw()
     end
   end
 
-  screen.color(table.unpack(color_scale(COL_RIPPLE, COL_DETECTOR_BG, math.min(pct_bang * 1.5, 1))))
+  -- ripples in detector
+  screen.color(table.unpack(color_scale(COL_RIPPLE, COL_DETECTOR_BG, math.min(pct_bang / FADE_T_RIPPLES, 1))))
   for _, r in pairs(ripples) do
     local angle, offset, radius = table.unpack(r)
     local r = radius * outer_r * 3/4
     local ax = center_x + (offset * outer_r) * cos(angle) * -1
     local ay = center_y + (offset * outer_r) * sin(angle)
 
-    -- screen.move(ax, ay)
-    -- screen.circle(radius * outer_r * 3/4)
+    -- if frame_count < 5 then
+    --   screen.move(ax, ay)
+    --   screen.circle(radius * outer_r * 3/4)
+    -- end
 
     for i=1,100 do
       if math.random(3) > 1 then
@@ -211,7 +234,8 @@ function redraw()
     end
   end
 
-  screen.color(table.unpack(color_scale(COL_FLASH, COL_PARTICLE, math.min(pct_bang * 4, 1))))
+  -- particles (orange curves)
+  screen.color(table.unpack(color_scale(COL_FLASH, COL_PARTICLE, math.min(pct_bang / FADE_T_PARTICLES, 1))))
   for _, p in pairs(particles) do
     local angle, angle2, dir = table.unpack(p)
     local bx = center_x + outer_r * cos(angle) * -1
@@ -223,7 +247,8 @@ function redraw()
     screen.curve(midx, midy, midx, midy, bx, by)
   end
 
-  screen.color(table.unpack(color_scale(COL_BARS, COL_BG, pct_bang * 2)))
+  -- hadrons (outer bars)
+  screen.color(table.unpack(color_scale(COL_BARS, COL_BG, pct_bang / FADE_T_HADRONS)))
   local offset = 0.01
   for _, h in pairs(hadrons) do
     local angle, amp = table.unpack(h)
@@ -251,30 +276,24 @@ function redraw()
     screen.line(bx, by)
   end
 
+  -- muons (red lines)
   screen.move(center_x, center_y)
-  screen.color(table.unpack(color_scale(COL_MUON, COL_BG, pct_bang * 1/4)))
+  screen.color(table.unpack(color_scale(COL_MUON, COL_BG, pct_bang / FADE_T_MUONS)))
   for _, angle in pairs(muons) do
     local bx = center_x + (screen_w/2) * cos(angle) * -1
     local by = center_y + (screen_w/2) * sin(angle)
     screen.line(bx, by)
   end
 
+  -- detector boundaries
   screen.move(center_x, center_y)
   screen.color(table.unpack(COL_BORDER))
   screen.circle(outer_r)
   screen.circle(inner_r)
-
-  if frame_count < 5 then
-    screen.move(1, 1)
-    screen.color(table.unpack(COL_FLASH))
-    screen.rect_fill(screen_w, screen_h)
-  end
 
   if seamstress then
     screen.refresh()
   elseif norns then
     screen.update()
   end
-
-  screen_dirty = false
 end
